f0b953aa9898dbbb60a833c6e285eb1b93950eda
Peter Duijnstee <peter@commuun.nl>
2020-09-16 10:37:32 +0200

    Rubocop fix <3

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
-require "active_record/railtie"
-require "action_controller/railtie"
+require 'active_record/railtie'
+require 'action_controller/railtie'
 
 Bundler.require(*Rails.groups)
 
